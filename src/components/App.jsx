f9a77b0e2bc8073da14a766b9710cfc42fa7a5b6
JaRcomGitHub <jarcom@ukr.net>
2022-11-21 21:06:44 +0200

    correction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,8 +19,8 @@ export const App = () =>  {
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />}></Route>
-        <Route path="/movies" element={<Movies />}></Route>
-        <Route path="/movies/:movieId" element={<MovieDetails />}>
+        <Route path="movies" element={<Movies />}></Route>
+        <Route path="movies/:movieId" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />}></Route>
           <Route path="reviews" element={<Reviews />}></Route>
         </Route>
